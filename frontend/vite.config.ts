2c34c497eff422985e9071b3bfb4d8c406889667
Javid Ahmed <javidahmed@icloud.com>
2026-07-15 23:01:24 +0100

    Add dependencies for react-query and react-colorful: update package.json and package-lock.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,22 @@
+import path from "path";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     outDir: "../static",
     emptyOutDir: true,
   },
   server: {
     proxy: {
-      "/login": "http://localhost:8000",
-      "/logout": "http://localhost:8000",
-      "/protected": "http://localhost:8000",
+      "/api": "http://localhost:8000",
     },
   },
 });
